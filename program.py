e365ce419e663f759a2ba1ec0a87b4c75d77c785
mirrazinmahmud <mirrazinmahmud88@gmail.com>
2022-09-07 21:42:17 +0600

    first commit

---
+++ program.py
@@ -0,0 +1,6 @@
+fname = input("enter first name: ")
+mname = input("enter middle name: ")
+lname = input("enter last name: ")
+
+fullname = fname + "."+ mname +"."+ lname + "@gmail.com"
+print("Fullname:", fullname)
